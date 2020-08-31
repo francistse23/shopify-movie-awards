5d1718fbd71ee91f6d52a6bcf8e2f22f31596636
francistse-realm <wtse.ucsd@gmail.com>
2020-08-31 11:07:36 -0700

    Adding search bar/function to page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const OMDB_KEY = process.env.REACT_APP_OMDB_KEY;
 
 function App() {
+  const [query, setQuery] = useState("");
+  // const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+
+  async function searchMovies(query) {
+    try {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${OMDB_KEY}&s=${query}&type=movie&plot=full`
+      );
+      const movies = await res.json();
+
+      console.log(movies);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Shoppies Nominations</h1>
+      <form className="Seach" onSubmit={() => searchMovies(query)}>
+        <label for="search">Movie Name </label>
+        <input
+          // inputMode="search"
+          type="text"
+          name="search"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="movie name"
+          value={query}
+        />
+      </form>
     </div>
   );
 }
