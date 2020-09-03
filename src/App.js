74f4a3cc82ae8877ebb0727ee5fb5a64b5597db5
francistse-realm <wtse.ucsd@gmail.com>
2020-09-03 14:21:33 -0700

    Added animation to handle notification banner when 5 nominations have been made.

--- src/App.js
+++ src/App.js
@@ -203,25 +203,31 @@ function App() {
         </div>
       </div>
 
-      {nominations.size === 5 && (
-        <footer
-          style={{
-            backgroundColor: "white",
-            position: "sticky",
-            bottom: 0,
-            lineHeight: "10vh",
-            height: "10vh",
-            borderRadius: "12px 12px 0 0",
-          }}
-        >
-          <h1>
-            You have made all 5 nominations!{" "}
-            <span aria-label="nominations" role="img">
-              🏆
-            </span>
-          </h1>
-        </footer>
-      )}
+      <CSSTransitionGroup
+        transitionName="nominations-footer"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+      >
+        {nominations.size === 5 && (
+          <footer
+            style={{
+              backgroundColor: "white",
+              position: "sticky",
+              bottom: 0,
+              lineHeight: "10vh",
+              height: "10vh",
+              borderRadius: "12px 12px 0 0",
+            }}
+          >
+            <h1>
+              You have made all 5 nominations!{" "}
+              <span aria-label="nominations" role="img">
+                🏆
+              </span>
+            </h1>
+          </footer>
+        )}
+      </CSSTransitionGroup>
     </div>
   );
 }
